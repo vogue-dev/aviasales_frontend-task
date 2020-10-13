d054e49ad19ca495dd77195500d61f0d1435056d
Vogue <oprya1111@yandex.ru>
2020-10-13 14:13:23 +0300

    Updates

---
+++ precache-manifest.f5f561d37a5e514174fc325d4476ec6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb5b5b4b9d2c47021ceeb0fadc13bd8",
+    "url": "/aviasales_frontend-task/index.html"
+  },
+  {
+    "revision": "c4fe3a48b0f4443ed3d0",
+    "url": "/aviasales_frontend-task/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ff0226bdd390fc669c40",
+    "url": "/aviasales_frontend-task/static/css/main.2c28823e.chunk.css"
+  },
+  {
+    "revision": "c4fe3a48b0f4443ed3d0",
+    "url": "/aviasales_frontend-task/static/js/2.d28dd3ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aviasales_frontend-task/static/js/2.d28dd3ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0226bdd390fc669c40",
+    "url": "/aviasales_frontend-task/static/js/main.0cb42266.chunk.js"
+  },
+  {
+    "revision": "3ee9e6cfd7f1a8b7109d",
+    "url": "/aviasales_frontend-task/static/js/runtime-main.7891542c.js"
+  }
+]);
